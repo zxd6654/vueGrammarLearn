a09956e32891d5b3fc0c196a3ec8e35149eca1c2
zxd6654 <739971915@qq.com>
2020-09-25 14:01:10 +0800

    vue3.x

---
+++ vue3.x/3.工具/2.Vue CLI/n.配置参考/babel.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: zxd 
+ * @Date: 2019-09-16 17:01:35 
+ * @Last Modified by: zxd
+ * @Last Modified time: 2019-09-16 17:17:13
+ */
+
+//Babel 可以通过 babel.config.js 进行配置。我们推荐在vue cli项目中始终使用babel.config.js取代其他格式
+//详情参考babel官网
+module.exports = {
+  presets: [
+    '@vue/app'
+  ]
+}
